1e2491534ba14dc0aca5dc33b79061ed455a1282
Luke Bergen <LukeABergen@gmail.com>
2012-11-19 00:36:08 -0500

    making map initialization param optional

--- application/map.rb
+++ application/map.rb
@@ -2,13 +2,15 @@ require 'json'
 
 class Map
   attr_accessor :name, :height, :width, :tileset, :tile_size, :tiles
-  def initialize(json)
+  def initialize(json=nil)
     # based on the json, create
-    map_hash = JSON.load(json)
-    @name = map_hash["name"]
-    @height = map_hash["height"]
-    @width = map_hash["width"]
-    @tiles = map_hash["tiles"]
-    @tileset = map_hash["tileset"]
+    if json
+      map_hash = JSON.load(json)
+      @name = map_hash["name"]
+      @height = map_hash["height"]
+      @width = map_hash["width"]
+      @tiles = map_hash["tiles"]
+      @tileset = map_hash["tileset"]
+    end
   end
 end
