1cd5dd2cd62ca227be41dcad2a2a21947f975c10
Luke Bergen <LukeABergen@gmail.com>
2012-11-19 01:11:37 -0500

    making a map editor

---
+++ dev/map_editor.rb
@@ -0,0 +1,162 @@
+APPLICATION_DIR = "./application"
+require 'gosu'
+require 'json'
+require 'debugger'
+require File.join([APPLICATION_DIR, "tileset.rb"])
+require File.join([APPLICATION_DIR, "map.rb"])
+
+class GameWindow < Gosu::Window
+
+  def initialize(name, height=nil, width=nil, tileset_name=nil)
+    super 1280, 800, false
+    @text = nil
+    @timer = 0
+    @dialog_font = Gosu::Font.new(self, Gosu::default_font_name, 20)
+    @mouse_img = Gosu::Image.new(self, "#{APPLICATION_DIR}/media/system/mouse.png")
+    if (height)
+      @map = Map.new
+      @map.name = name
+      @map.height = height.to_i
+      @map.width = width.to_i
+      @map.tileset = tileset_name
+      @map.tiles = []
+      @map.height.times do
+        arr = []
+        @map.width.times do
+          arr << []#[0,0,0,0,0]
+        end
+        @map.tiles << arr
+      end
+    else
+      json = File.read(File.join([".", name, "data.json"]))
+      @map = Map.new(json)
+    end
+    unless tileset_name
+      tileset_name = @map.tileset
+    end
+    tileset_dir = File.join([APPLICATION_DIR, "tilesets", tileset_name])
+    @tileset = Tileset.new(self, tileset_dir)
+    @selected_tile = [0,0]
+    @currently_over = [0,0]
+  end
+
+  def update
+    set_currently_over
+  end
+
+  def draw
+    set_background
+    draw_mouse
+    draw_selected_tile
+
+    ts = @tileset.tile_size
+    @map.tiles.each_with_index do |row, y|
+      row.each_with_index do |data_arr, x|
+        next if data_arr.empty?
+        img = @tileset.tiles[data_arr[0]][data_arr[1]]
+        img.draw(x*ts, y*ts, 100)
+      end
+    end
+
+    if @text
+      @dialog_font.draw(@text, 0, 0, 0, 1, 1, Gosu::Color::BLACK)
+      if (@timer <= 0)
+        @text = nil
+      else
+        @timer -= 1
+      end
+    end
+  end
+
+  def button_down(id)
+    if id == Gosu::KbEscape
+      close
+    end
+    if id == Gosu::KbD
+      do_debugger
+    end
+    if id == Gosu::KbS
+      notify("Saving")
+      json = @map.to_json
+      unless File.exists?("./#{@map.name}")
+        Dir.mkdir("./#{@map.name}")
+      end
+      File.open("./#{@map.name}/data.json", 'w') {|f| f.write(json)}
+    end
+    if id == Gosu::KbT
+      notify("Selecting Tile")
+    end
+    if id == Gosu::KbO
+      notify("Selecting Object")
+    end
+    if id == Gosu::KbX
+      notify("currently over: #{@currently_over[0]}, #{@currently_over[1]}")
+    end
+    if id == Gosu::MsLeft
+      notify("Left-Click")
+      previous_tile = @map.tiles[@currently_over[1]][@currently_over[0]]
+      previous_tile[0] = @selected_tile[0]
+      previous_tile[1] = @selected_tile[1]
+      @map.tiles[@currently_over[1]][@currently_over[0]] = previous_tile
+    end
+    if id == Gosu::MsRight
+      notify("Right-Click")
+    end
+  end
+
+  def notify(text)
+    @text = text
+    @timer = 60
+  end
+
+  def do_debugger
+    debugger
+    noop = nil
+  end
+
+  def draw_mouse
+    @mouse_img.draw(self.mouse_x, self.mouse_y, 10000)
+  end
+
+  def draw_selected_tile
+    @selected_tile[0]
+    @selected_tile[1]
+    ts = @tileset.tile_size
+    img = @tileset.tiles[@selected_tile[0]][@selected_tile[1]]
+    img.draw(@currently_over[0]*ts, @currently_over[1]*ts, 100)
+  end
+
+  def set_currently_over
+    ts = @tileset.tile_size
+    x = self.mouse_x / ts
+    y = self.mouse_y / ts
+    @currently_over = [x.floor,y.floor]
+  end
+
+  def set_background
+    w = Gosu::Color::WHITE
+    width = @map.width * @tileset.tile_size
+    height = @map.height * @tileset.tile_size
+    self.draw_quad(0, 0, w, 
+                      width, 0, w,
+                      width, height, w,
+                      0, height, w)
+  end
+end
+
+# "name":"dev_area","height":64,"width":64,"tileset":"tiles","tiles"
+if ARGV[0] && ARGV[1] && ARGV[2] && ARGV[3]
+  name, height, width, tileset_name = ARGV[0], ARGV[1], ARGV[2], ARGV[3]
+  win = GameWindow.new(name, height, width, tileset_name)
+  win.show
+elsif ARGV[0]
+  name = ARGV[0]
+  win = GameWindow.new(name)
+  win.show
+else
+  puts "Usage:"
+  puts "ruby map_editor.rb 'map-name' 'height' 'width' 'tileset-to-use'"
+  puts "OR"
+  puts "ruby map_editor.rb 'name-of-existing-map'"
+  exit
+end
